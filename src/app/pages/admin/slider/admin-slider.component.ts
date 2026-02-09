caecb7983b4e9491dfa282fdc010c4035c433a29
GeoCo <beqacalani@gmail.com>
2026-02-09 16:59:57 +0400

    add admin slider component

---
+++ src/app/pages/admin/slider/admin-slider.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-admin-slider-page',
+  standalone: true,
+  imports: [],
+  templateUrl: 'admin-silder.component.html'
+})
+
+export class AdminSliderPageComponent {
+
+}
