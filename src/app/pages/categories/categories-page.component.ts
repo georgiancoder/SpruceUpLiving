f457c1f758a457527d538ded6b2e32e8da4b8d73
GeoCo <beqacalani@gmail.com>
2026-02-10 18:40:31 +0400

    categories page

---
+++ src/app/pages/categories/categories-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-categories-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './categories-page.component.html',
+  styleUrl: './categories-page.component.css',
+})
+export class CategoriesPageComponent implements OnInit {
+  async ngOnInit() {
+  }
+}
+
--- src/app/pages/main/main-page.component.ts
+++ src/app/pages/main/main-page.component.ts
@@ -16,4 +16,3 @@ export class MainPageComponent {
   @Input() title: string = 'Welcome to the Main Page';
 
 }
-
