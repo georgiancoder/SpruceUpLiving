f90dcdf74485441af468a4de8efb4a0ebe40c9e3
GeoCo <beqacalani@gmail.com>
2026-02-13 15:03:51 +0400

    search

--- src/app/components/categories-header/categories-header.component.ts
+++ src/app/components/categories-header/categories-header.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import {it} from 'vitest';
 
 @Component({
   selector: 'app-categories-header',
@@ -12,9 +11,9 @@ export class CategoriesHeaderComponent {
   @Input() title = 'Categories';
   @Input() subtitle = 'Browse all categories.';
 
-  // Optional action (e.g. "Back home")
   @Input() actionText: string | null = 'Back home';
   @Input() actionLink: string | null = '/';
-  protected readonly it = it;
-}
 
+  @Input() query = '';
+  @Output() queryChange = new EventEmitter<string>();
+}
